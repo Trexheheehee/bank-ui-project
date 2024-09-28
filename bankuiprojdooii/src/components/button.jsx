0823ae275af3f1fb14d6df173312d7a92e961e5c
Trexheheehee <postboxno.000111@gmail.com>
2024-09-28 13:46:31 +0530

    hi

---
+++ bankuiprojdooii/src/components/button.jsx
@@ -0,0 +1,11 @@
+
+
+const Button = () => {
+  return (
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-10 rounded-[10px]`}>
+      GetStarted
+    </button>
+  )
+}
+
+export default Button
---
+++ bankuiprojdooii/src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./billing";
+import CardDeal from "./cardDeal";
+import Business from "./business";
+import Clients from "./clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./footer";
+import Testimonials from "./Testimonials";
+import Hero from "./hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
